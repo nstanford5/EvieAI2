69b6e64ba67473a4844e87a60fccfad1eafa75c6
Nick Stanford <nick@reach.sh>
2022-10-25 09:01:26 -0600

    more words

--- events/message.js
+++ events/message.js
@@ -5,10 +5,23 @@
  * 
  * 
  * Features implemented:
- * Map, Set, Types, Feedback, docs, faucets, parallelReduce, API
- *
- * To be implemented:
- * REACH_ISOLATED_NETWORK, enum
+ * Map, Set, Types, Feedback,views,  docs, faucets, parallelReduce, API, enum,
+ * docker, jpmiller, nftMami, beard vs. mustache, deploy(), ParticipantClass,
+ * balance zero at exit, install reach, iterate through maps, how do i consume map values,
+ * deploying with no test tokens error, reach update/releases channel, Bytes,
+ * balance insufficient for transfer, expected x args got y, invalid unbound identifier,
+ * what are gas fees, steps, local step, consensus step, Participant Interact Interface,
+ * 
+ * 
+ * 
+ * Common errors/to be implemented:
+ * REACH_ISOLATED_NETWORK
+ * compiled backend doesn't meet the expectations of the reach standard library
+ * api called in wrong state
+ * check > asumme/require
+ * difference between testnet, mainnet, devnet
+ * 
+ * 
  * 
  * Author: Nick Stanford
  * Date: 2 October 2022
@@ -21,7 +34,7 @@ module.exports = {
     if(message.content.includes("map") && (!message.content.includes("value") && (!message.content.includes("iterate")))) {
       await message.reply("A Map is a set of key:value pairs.\n" +
         "On Algorand all maps must have an Address type as their key. This restriction will soon be lifted.\n" +
-        "Maps are the only variable sized container in Reach.\n" +
+        "Maps are the only variably sized container in Reach.\n" +
         "They must be created in a consensus step.\n" +
         "The syntax to create a Map is `const pMap = new Map(Address, UInt)` "+
         "If the first argument is left blank it will default to Address type\n" +
@@ -36,7 +49,7 @@ module.exports = {
         "To add to a Set use `pSet.insert(Address)` \n" +
         "If storing users from an API call use `pSet.insert(this)` \n")
       return;
-    } else if (message.content.includes('type')){
+    } else if (message.content.includes('type') || message.content.includes('types') || message.content.includes('Types')){
       await message.reply("These are the available Types in Reach \nNull\nBool - boolean\nUInt - unsigned integer." +
         "UInt.max is the largest value allowed\nUInt256 - unsigned integer with 256 bits. UInt256.max is largest value " + 
         "allowed\nInt - signed integer\nFixedPoint - fractional number\nBytes(length) - sequence of bytes of at most length." + 
@@ -68,13 +81,14 @@ module.exports = {
     } else if (message.content.includes('iterate') && (message.content.includes('map') || message.content.includes('maps') || message.content.includes('Map')) || message.content.includes('Maps')){
       await message.reply("You cannot iterate over a Map outside of an `invariant`" +
         "The reason for this is that Maps are variably sized -- they could theoretically hold one entry "+
-        "for every account on the network. If we allowed you iterate through this variably sized container " +
+        "for every account on the network. If we allowed you to iterate through this variably sized container " +
         "you could exceed network computation limits.\n" +
         "The fix here is to do what you need incrementally. If you need to iterate through the Map to pay everyone, "+
         "you would instead create a function where each user could claim payouts individually");
       return;
     } else if (message.content.includes('help') && message.content.includes('error')){
-      await message.reply("If you type one of the keywords you are having trouble with, I can share more information");
+      await message.reply("If you type one of the keywords you are having trouble with, I can share more information. \n" +
+        "Typing the exact error message will return a solution");
       return;
     } else if (message.content.includes('ApplicationCreate txn')){
       await message.reply("Did you fund this account with testnet tokens?\n" +
@@ -85,9 +99,9 @@ module.exports = {
       await message.reply("`Bytes(length)` is the type in Reach for strings, it takes one argument for the length. You must fix the length and when matching bytes constants, their lengths must match\n" +
         "You can pad Bytes to desired length: https://docs.reach.sh/rsh/compute/#padding \n" +
         "You can use `BytesDyn` to denote a dynamically sized collection of bytes, but this is imcompatible with static Bytes types \n" +
-        "Link to Docs on Bytes type: https://docs.reach.sh/rsh/compute/#p_62");
+        "Link to Docs on Bytes type: https://docs.reach.sh/rsh/compute/#p_62 \n");
       return;
-    // feeback collection
+    // feedback collection
     } else if (message.content.includes('could be better') || message.content.includes('feedback')) {
       await message.reply("Thanks for the feedback. Please submit in detail here: [link to feedback form]\n" + 
         "We value your feedback and it helps the Reach language grow.\n" +
@@ -132,6 +146,7 @@ module.exports = {
       await message.reply("Yes, I know the infamous NFTMami!\n" +
         "Twitter: @NFTMami\n" +
         "Discord: Thuglif3#8365\n");
+      return;
     } else if(message.content.includes('install Reach') || message.content.includes('install reach') || message.content.includes('installing Reach')) {
       await message.reply("Here is some information about installing Reach\n" +
         "Docs: https://docs.reach.sh/quickstart/ \n" +
@@ -140,9 +155,9 @@ module.exports = {
         "Make sure to follow instructions carefully! There are a few key points that can cause trouble (i.e. 'Run as Administrator')");
       return;
     } else if(message.content.includes("jpmiller") || message.content.includes('jp miller')){
-        await message.reply("Oh, yea -- I know JP!\n" +
-          "Twitter: @therealjpmiller\n" +
-          "Discord: JP Miller#8084\n");
+      await message.reply("Oh, yea -- I know JP!\n" +
+        "Twitter: @therealjpmiller\n" +
+        "Discord: JP Miller#8084\n");
       return;
     } else if ((message.content.includes("API") || message.content.includes('api')) && message.content.includes('member') || message.content.includes('interaction')){
       await message.reply("APIs are functions that can be called by other contracts as well as off chain.\n" +
@@ -157,12 +172,66 @@ module.exports = {
       return;
     } else if (message.content.includes('docker') || message.content.includes('Docker')) {
       await message.reply("Docker creates containerized environments for development. It allows us to create local blockchains for testing in devnet environments.\n" +
-        "Docker docs: https://docs.docker.com/ \n");
+        "Docker docs: https://docs.docker.com/ \n\n" +
+        "It is good practice to set down old docker containers occasionally with `reach down`");
       return;
     } else if (message.content.includes('how') && (message.content.includes('txn costs') || message.content.includes('debug'))) {
       await message.reply("You can use the `REACH_DEBUG` feature to output useful information about your application.\n" +
         "To turn on the debug once use `REACH_DEBUG=Y ./reach compile`" +
         "Or you can `export REACH_DEBUG=Y` to export it as an environment variable\n");
+      return;
+    } else if((message.content.includes("how") || message.content.includes("How")) && message.content.includes("update")){
+      await message.reply("To update Reach you should run `reach update` in the correct directory.\n" +
+        "If you are having troubles with an update, see the #releases channel for more information.\n" +
+        "Check your docker image hashes with `reach hashes` they should match the #releases channel exactly\n" + 
+        "For more information on new features in an update, see the changelog: https://docs.reach.sh/changelog/#guide-changelog \n");
+      return;
+    } else if (message.content.includes('balance') && (message.content.includes('zero') || (message.content.includes('exit')) )) {
+      await message.reply("This is a built in verification check in Reach. It means that Reach tried to prove that the balance of the contract is zero at the end of its execution. i.e. are there still tokens in the contract account. " +
+        "Carefully track the balance of your contract and ensure that all tokens have been transferred out before the call to `exit`");
+      return;
+    } else if(message.content.includes('insufficient') && message.content.includes('balance') && message.content.includes('transfer')){
+      await message.reply("This means the contract did not have enough tokens to complete the transfer. Check the account balances, pay amounts, and carefully track the balance of the contract account\n");
+      return;
+    } else if(message.content.includes('expected') && message.content.includes('args') && message.content.includes('got')){
+      await message.reply("This means that the number of inputs to your function don't match it's signature.\n" +
+        "Check your function signature in the Participant Interact Interface or Shared Interface Object\n" +
+        "More info here: https://docs.reach.sh/rsh/errors/#RE0000 \n");
+      return;
+    } else if(message.content.includes('invalid') && message.content.includes("unbound") && message.content.includes('identifier')){
+      await message.reply("This typically means that you are trying to use a value that has not been published or assigned appropriately\n" +
+        "Check that the value you are using has been assigned and published.\n");
+      return;
+    } else if((message.content.includes('What') || message.content.includes("what")) && message.content.includes("gas") && message.content.includes('fees')){
+      await message.reply("Gas fees are transaction costs on a given network. Each time we need to `publish` or `pay` the network charges " +
+        "that user a transaction fee. On Algorand they are fixed at .001 algo, on ETH L1 they are variable based on network congestion and most ETH L2s have some fixed low transaction cost\n");
+      return;
+    } else if((message.content.includes('Participant Interact Interface') || message.content.includes('participant interact interface')) && (message.content.includes('what') || message.content.includes('What'))){
+      await message.reply('The Participant Interact Interface is where we specify the function signatures we are allowing for a Participant. Sometimes we create an object to share functions between users.');
+      return;
+    } else if ((message.content.includes("local") || message.content.includes('Local')) && (message.content.includes('What') || message.content.includess('what')) && (message.content.includes('step') || message.content.includes('Step'))){
+      await message.reply('Local Steps are between the user and their computer. This is how we gather inputs from individuals interacting privately until we publish the information to the blockchain.\n\n' +
+      "More info here: https://docs.reach.sh/model/#p_27 \n");
+      return;
+    } else if ((message.content.includes('Consensus Step') || message.content.includes('consensus step')) && (message.content.includes('What') || message.content.includes('what'))){
+      await message.reply('Consensus steps are blockchain interactions from values in the contract\n\n' +
+      "The word consensus means 'everyone agrees'\n" +
+      "These steps are the parts of the program where participants interact on the blockchain to agree upon a shared truth.\n" +
+      "This is where information is written to the Blockchain.\n");
+      return;
+    } else if ((message.content.includes('what') || message.content.includes('What')) && (message.content.includes('step') || message.content.includes('Step'))) {
+      await message.reply("Step = a different node in the state diagram of the program.\n" +
+      "Typically, during Steps the program is gathering information and/or waiting for an event(i.e. Alice waits for Bob to select his move)\n\n" +
+      "More info here: https://github.com/reach-sh/reach-lang/discussions/1171 \n");
+      return;
+    } else if(message.content.includes('launch') && message.content.includes('token')){
+      await message.reply('launchToken is a standard library function in Reach. You would use `stdlib.launchToken(acc, name, identifier, supply)`\n' +
+        "Supply, if left out, will default to the max allowed on the network.\n\n" +
+        "More info here: https://docs.reach.sh/frontend/#js_stdlib.launchToken \n");
+      return;
+    } else if((message.content.includes('Testnet') || message.content.includes('testnet')) && (message.content.includes('What') || message.content.includes('what'))){
+      await message.reply('Testnet is an open public testing environment. It will allow you to deploy your DApp and test wallet interactions or allow users to test your DApp in an environment where tokens have no value before moving to Mainnet\n' +
+      'Testnet does not allow `newTestAccount` you must switch to `getDefaultAccount` or some other supported account retrieval function\n');
       return;
     } else {
       // could add a return here to not respond to messages without triggers
